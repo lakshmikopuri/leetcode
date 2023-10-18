90ae47ae257e7f9a4ca61cee85e501a69a7c7a1f
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-10-18 15:04:20 +0530

    Update Power of Three.py

--- Power of Three.py
+++ Power of Three.py
@@ -1,7 +1,7 @@
 #Power of Three
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        for i in range((n)):
+        for i in range(20):
             if n==3**i:
                 return True
                 break
