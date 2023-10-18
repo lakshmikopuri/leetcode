73a16c9efa9be17dccd25c2e66cbc19558ab7771
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-10-18 14:08:57 +0530

    Create  Length of Last Word.py

---
+++ Length of Last Word.py
@@ -0,0 +1,9 @@
+# Length of Last Word
+#Given a string s consisting of words and spaces, return the length of the last word in the string.A word is a maximal substring consisting of non-space characters only.
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        stripped = s.strip()
+        strList = stripped.split(" ")
+        lastWord = strList[-1]
+        return (len(lastWord))
+        
