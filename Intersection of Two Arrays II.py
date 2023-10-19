86486af02bfc5a06b940543efbdcd14eb6f2f579
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-10-19 10:36:42 +0530

    Create Intersection of Two Arrays II.py

---
+++ Intersection of Two Arrays II.py
@@ -0,0 +1,9 @@
+#Intersection of Two Arrays II
+#Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        l=[]
+        for i in nums1:
+            if i in nums2:
+                l.append(i)
+        return l
