952e856b271f15b1f53dfc93db6db8d4ece93e0a
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-10-20 11:09:41 +0530

    Create  Add Strings.py

---
+++ Add Strings.py
@@ -0,0 +1,7 @@
+# Add Strings
+#Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        sys.set_int_max_str_digits(10000)
+        sum_=int(num1)+int(num2)
+        return (str(sum_))
