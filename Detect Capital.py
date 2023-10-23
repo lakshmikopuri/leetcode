04403cf7cdebae90452d11cafdce5da82b918080
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-10-23 13:19:38 +0530

    Create Detect Capital.py

---
+++ Detect Capital.py
@@ -0,0 +1,7 @@
+#Detect Capital
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper() or word.islower() or word.istitle():
+            return True
+        else:
+            return False
