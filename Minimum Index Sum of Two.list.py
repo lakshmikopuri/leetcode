85b200009aa0eca67390b9ba2b487d922d80bdac
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-10-25 14:22:35 +0530

    Create  Minimum Index Sum of Two.list.py

---
+++ Minimum Index Sum of Two.list.py
@@ -0,0 +1,16 @@
+# Minimum Index Sum of Two Lists
+#Given two arrays of strings list1 and list2, find the common strings with the least index sum.A common string is a string that appeared in both list1 and list2.A common string with the least index sum is a common string such that if it appeared at list1[i] and list2[j] then i + j should be the minimum value among all the other common strings.Return all the common strings with the least index sum. Return the answer in any order.
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        d={}
+        result=[]
+        res=len(list1)+len(list2)
+        for i in list1:
+            if i in list2:
+                a=list1.index(i)+list2.index(i)
+                res=min(res,a)
+                d[i]=a
+        for k,v in d.items():
+            if v==res:
+                result.append(k)
+        return result
