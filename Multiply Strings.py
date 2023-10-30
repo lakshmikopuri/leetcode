db8fd2d3c6059e9049c23734d713163ae08b9033
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-10-30 15:25:10 +0530

    Create Multiply Strings.py

---
+++ Multiply Strings.py
@@ -0,0 +1,5 @@
+#Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        result= int(num1)*int(num2)
+        return str(result)
